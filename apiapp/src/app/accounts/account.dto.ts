b33e497cbb7d16c58d9d9ea43c27a2fa37e27f36
Liliia Mac <echo.dev1992@gmail.com>
2025-09-09 09:27:55 +0100

    account dto updated

--- apiapp/src/app/accounts/account.dto.ts
+++ apiapp/src/app/accounts/account.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class AccountDto {
-  @ApiProperty({ example: 'Pomazuevaass' })
+  @ApiProperty({ example: 'Pomazuevaa' })
   @IsNotEmpty()
   @IsString()
   name: string;
